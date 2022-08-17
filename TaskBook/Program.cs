a2f6b9f57a8aedaea4cabe9f8b761cfa3134617e
Postrik <postrik@list.ru>
2022-08-17 18:54:19 +0500

    add Task Book

---
+++ TaskBook/Program.cs
@@ -0,0 +1,80 @@
+﻿//Напишите программу для вывода своих фамилии, имени и отчества на каждой новой строке.
+/*
+System.Console.Write("Введите своё имя: ");
+string name = Console.ReadLine();
+
+System.Console.Write("Введите своё отчество: ");
+string patronymic = Console.ReadLine();
+
+System.Console.Write("Введите свою фамилию: ");
+string surname = Console.ReadLine();
+*/
+
+// Вывести на экран пять строк из нулей, причем количество нулей в каждой строке
+// равно номеру строки. Использовать только Console.Write().
+
+//System.Console.Write("0\n00\n000\n0000\n00000");
+// "\n" управляющий символ перевода курсора на новую строчку
+
+//Console.Write(5 + " 11 1025");
+//Console.Write(5 + " 11 1025");
+//Console.Write("5 11" + 1025);
+//Console.Write(5 + " " + 11 + " " + 1025);
+
+// Home_1. Составить программу вывода на экран в одну строку трех любых чисел
+//с двумя пробелами между ними.
+// System.Console.WriteLine("Введите 3 числа: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+
+// Home_2. Составить программу вывода на экран в одну строку четырех любых чисел
+// с одним пробелом между ними.
+//System.Console.Write(1 + " " + 2 + " " + 3 + " " + 4);
+
+//Home_3. Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними.
+//System.Console.WriteLine(1 + " " + 13 + " " + 49);
+
+//Home_4.Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.
+//System.Console.WriteLine(7 + "  " + 15 + "  " + 100);
+
+//Home_5. Вывести на экран числа 50 и 10 одно под другим.
+// System.Console.WriteLine(50);
+// System.Console.WriteLine(10);
+
+//Home_6. Вывести на экран числа 5, 10 и 21 одно под другим.
+// System.Console.WriteLine(5);
+// System.Console.WriteLine(10);
+// System.Console.WriteLine(21);
+
+//Home_7. Составить программу вывода на экран «столбиком» четырех любых чисел
+// System.Console.WriteLine("Введите 4 числа: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// int number4 = Convert.ToInt32(Console.ReadLine());
+
+// Home_8. Вывести на экран текущее название дня недели,
+// название месяца и свое имя. Каждое слово должно быть в отдельной строке.
+// System.Console.WriteLine("Среда");
+// System.Console.WriteLine("Август");
+// System.Console.WriteLine("Павел");
+
+// Home_9. Вывести на экран прямоугольник, заполненный буквами А.
+// Количество строк в прямоугольнике равно 5, количество столбцов равно 8.
+// System.Console.WriteLine("AAAAAAAA\n"+
+//                          "AAAAAAAA\n"+
+//                          "AAAAAAAA\n"+
+//                          "AAAAAAAA\n"+
+//                          "AAAAAAAA");
+
+//Home_10. Вывести на экран букву «W» из символов «*» (звездочка).
+// System.Console.WriteLine("*       *\n"+
+//                          " *  *  *\n"+
+//                          "  *   *");
+
+// Объявите переменную с именем myPerson строкового типа и запишите в неё свое имя
+// на первой строке, а фамилию на второй строке.
+
+string myPerson = "Pavel\nOstrik";
